9b89c1ed4d74b692d04e9cc5944bc7646be4fc74
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2023-03-18 17:36:00 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -28,9 +28,10 @@ successMessage.innerHTML = `<div>
 <p class="mb-0">Your home city: <span class="font-weight-bold">${sessionStorage.h}</span></p>
 <p class="">Your unique access code: <span class="font-weight-bold">${sessionStorage.h.substring(0, 3).toLowerCase()}${sessionStorage.m.replaceAll(" ","")}</span></p>
 <p>Thank you.</p>
-            <button class="btn btn-primary mb-3" onclick="window.print()" style="
+            <button class="btn btn-primary mb-3 " onclick="window.print()" style="
     display: flex;
     align-items: center;
+    float: right;
 "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer mr-2" viewBox="0 0 16 16">
   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"></path>
   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"></path>
